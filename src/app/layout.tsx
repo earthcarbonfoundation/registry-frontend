2da0eef1405e4362d245533acc11d53ed59bc899
nishith003 <nishithshah03@gmail.com>
2026-02-07 13:37:59 +0530

    fix: better user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,23 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@700&display=swap'
+          rel='stylesheet'
+        />
+      </head>
       <body>
         <AuthProvider>
           {children}
-          <ToastContainer position="top-right" autoClose={3000} />
+          <ToastContainer position='top-right' autoClose={3000} />
         </AuthProvider>
       </body>
     </html>
